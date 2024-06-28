37ba421acd222ea6af72322420aa24f2979473bc
Jeydin21 <jeydin@j21.dev>
2024-06-28 01:32:22 -0500

    chore: Added 404 page

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ function Home() {
   return (
     <MainLayout>
       <div className="flex flex-col min-h-screen justify-center text-center space-y-5">
-        <div className="text-light-text dark:text-dark-text font-semibold text-4xl sm:text-7xl">Hello, I'm Jeydin!</div>
+        <div className="text-light-text dark:text-dark-text font-semibold text-4xl sm:text-7xl">Hello, I&apos;m Jeydin!</div>
         <div className="text-light-text dark:text-dark-text font-extralight text-xl sm:text-3xl">Student | Anime Enjoyer | Linux Enthusiast</div>
         <ButtonsContainer />
       </div>
