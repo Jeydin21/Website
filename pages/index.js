b4c7461b215ab5edcfd3c6c55a79093b53d3fbc9
Jeydin21 <jeydin@j21.dev>
2024-10-11 23:33:07 -0500

    Added profile icon

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
 import MainLayout from "../components/layout/MainLayout";
 import ButtonsContainer from "../components/landing/ButtonsContainer";
+import Image from "next/image";
 
 function Home() {
   return (
     <MainLayout>
-      <div className="flex flex-col min-h-screen justify-center text-center space-y-5">
+      <div className="flex flex-col min-h-screen justify-center items-center text-center space-y-5">
+        <Image
+          src="/elaina.png"
+          alt="Jeydin"
+          width={200}
+          height={200}
+          className="rounded-full"
+        />
         <div className="text-light-text dark:text-dark-text font-semibold text-4xl sm:text-7xl">Hello, I&apos;m Jeydin!</div>
-        <div className="text-light-text dark:text-dark-text font-extralight text-xl sm:text-3xl">Student | Anime Enjoyer | Linux Enthusiast</div>
+        <div className="text-light-text dark:text-dark-text font-extralight text-xl sm:text-3xl">Student, Developer, Linux Enthusiast</div>
         <ButtonsContainer />
       </div>
     </MainLayout>
