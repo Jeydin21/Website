75a1cd5cab274980c6a88b7e8c9c31f37368a19d
Jeydin21 <JeyPham21@gmail.com>
2024-10-16 08:16:21 -0500

    Hide profile icon

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,13 @@ function Home() {
   return (
     <MainLayout>
       <div className="flex flex-col min-h-screen justify-center items-center text-center space-y-5">
-        <Image
+        {/* <Image
           src="/favicon.png"
           alt="Jeydin"
           width={200}
           height={200}
           className="rounded-full"
-        />
+        /> */}
         <div className="text-light-text dark:text-dark-text font-semibold text-4xl sm:text-7xl">Hello, I&apos;m Jeydin!</div>
         <div className="text-light-text dark:text-dark-text font-extralight text-xl sm:text-3xl">Student, Developer, Linux Enthusiast</div>
         <ButtonsContainer />
